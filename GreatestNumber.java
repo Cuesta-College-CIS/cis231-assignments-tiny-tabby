e22bc2769c2d1b4958b3ff071ff70b63c8c1245c
tiny-tabby <tinytabby66@gmail.com>
2022-02-07 17:35:30 -0800

    Find The Greatest Number

---
+++ GreatestNumber.java
@@ -0,0 +1,32 @@
+
+import java.util.Random;
+public class GreatestNumber{
+    public static void main (String[] args){
+        
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt(101);
+        int randomNum2 = rand.nextInt(101);
+        int randomNum3 = rand.nextInt(101);
+
+        System.out.println(randomNum + ", " + randomNum2 + ", " + randomNum3);
+    
+        
+        int greatestNum = 0;
+        if ( randomNum > randomNum2 && randomNum > randomNum3){
+             greatestNum = randomNum;
+        }
+        else{
+            greatestNum = randomNum2;
+
+            if (randomNum3 > randomNum2){
+                greatestNum = randomNum3;
+            }
+            else{
+                greatestNum = randomNum2;
+            }
+        }
+        
+        System.out.println("The greatest number is " + greatestNum);
+    }
+}
